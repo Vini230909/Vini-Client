2b43ccb2c4a061704e465d5656c30a9605f5bad8
Vini230909 <vini230909@gmail.com>
2026-05-12 20:00:10 +0200

    Initial Mindustry mod setup
    Set up Java mod template for TurretAutoFill and configured mod metadata.

---
+++ src/vini/turretautofill/TurretAutoFill.java
@@ -0,0 +1,15 @@
+package vini.turretautofill;
+
+import arc.util.Log;
+import mindustry.mod.Mod;
+
+public class TurretAutoFill extends Mod {
+    public TurretAutoFill() {
+        Log.info("TurretAutoFill loaded.");
+    }
+
+    @Override
+    public void loadContent() {
+        Log.info("TurretAutoFill content loaded.");
+    }
+}
